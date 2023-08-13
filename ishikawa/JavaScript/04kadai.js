2a6b6dc9d8251673cf8387d1b33da6707c6a2317
石川倖太 <ishikawakouta@korokoroMacBook-Air.local>
2023-08-13 11:28:05 +0900

    04kadai.js修正

--- ishikawa/JavaScript/04kadai.js
+++ ishikawa/JavaScript/04kadai.js
@@ -3,7 +3,7 @@ if (mon < 1) {
     console.log(mon + '月はJanuaryです');
 } else if (mon <= 2) {
     console.log(mon + '月はFebruaryです');
-} else if (mon = 3) {
+} else if (mon === 3) {
     console.log(mon + '月はMarchです');
 } else { 
     console.log(mon + '判定できません');
--- ishikawa/JavaScript/5_16.js
+++ ishikawa/JavaScript/5_16.js
@@ -6,3 +6,4 @@ for (let i = 10; i > 0; i--) {
     }
     console.log(s);
 }
+
